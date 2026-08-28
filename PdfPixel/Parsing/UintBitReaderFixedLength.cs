3030f0db3016fc884f810e86b353e488370d862a
zayg21-pixel <zayg21@gmail.com>
2026-08-28 22:36:06 +0200

    Optimize 1 bit images

--- PdfPixel/Parsing/UintBitReaderFixedLength.cs
+++ PdfPixel/Parsing/UintBitReaderFixedLength.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Buffers.Binary;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace PdfPixel.Parsing;
 
@@ -10,12 +11,17 @@ namespace PdfPixel.Parsing;
 /// </summary>
 internal ref struct UintBitReaderFixedLength
 {
+#if NET5_0_OR_GREATER
+    private ref byte _dataReference;
+#else
     private readonly ReadOnlySpan<byte> _data;
+    private int _bufferedByteIndex;
+#endif
     private readonly int _bitCount;
     private readonly int _inverseBitCount;
+    private int _bytesRemaining;
     private int _bufferedBits;
     private ulong _buffer;
-    private int _bufferedByteIndex;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="UintBitReaderFixedLength"/> struct.
@@ -36,11 +42,19 @@ internal ref struct UintBitReaderFixedLength
     /// <param name="startBit">Bit offset of the first value to read.</param>
     public UintBitReaderFixedLength(in ReadOnlySpan<byte> data, int bitCount, int startBit)
     {
+        int startByteIndex = Math.Min(startBit >> 3, data.Length);
+
+#if NET5_0_OR_GREATER
+        _dataReference = ref Unsafe.Add(ref MemoryMarshal.GetReference(data), startByteIndex);
+#else
         _data = data;
+        _bufferedByteIndex = startByteIndex;
+#endif
+
         _bitCount = bitCount;
         _inverseBitCount = 64 - bitCount;
+        _bytesRemaining = data.Length - startByteIndex;
         _buffer = 0;
-        _bufferedByteIndex = startBit >> 3;
 
         int misalignedBits = startBit & 7;
 
@@ -78,26 +92,45 @@ internal ref struct UintBitReaderFixedLength
     private void FillBuffer()
     {
         int bitsRemaining = 64 - _bufferedBits;
-        int bytesRemaining = _data.Length - _bufferedByteIndex;
-        int bytesToRead = Math.Min(bytesRemaining, bitsRemaining >> 3);
+        int bytesToRead = Math.Min(_bytesRemaining, bitsRemaining >> 3);
+
+#if NET5_0_OR_GREATER
+        ref byte dataReference = ref _dataReference;
+#else
+        ref byte dataReference = ref Unsafe.Add(ref MemoryMarshal.GetReference(_data), _bufferedByteIndex);
+#endif
 
         if (bytesToRead >= 8)
         {
-            // Only reachable when _bufferedBits == 0, so we can assign directly.
-            _buffer = BinaryPrimitives.ReadUInt64BigEndian(_data.Slice(_bufferedByteIndex, 8));
-            _bufferedByteIndex += 8;
+            ulong window = Unsafe.ReadUnaligned<ulong>(ref dataReference);
+            _buffer = (BitConverter.IsLittleEndian) ? BinaryPrimitives.ReverseEndianness(window) : window;
             _bufferedBits = 64;
         }
         else
         {
-            int endIndex = _bufferedByteIndex + bytesToRead;
-            for (int i = _bufferedByteIndex; i < endIndex; i++)
+            for (int byteOffset = 0; byteOffset < bytesToRead; byteOffset++)
             {
-                _buffer |= (ulong)_data[i] << (56 - _bufferedBits);
+                _buffer |= (ulong)Unsafe.Add(ref dataReference, byteOffset) << (56 - _bufferedBits);
                 _bufferedBits += 8;
             }
-
-            _bufferedByteIndex = endIndex;
         }
+
+        Advance(bytesToRead);
+    }
+
+    /// <summary>
+    /// Moves the read position forward by the given number of bytes.
+    /// </summary>
+    /// <param name="byteCount">The number of bytes consumed from the data.</param>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private void Advance(int byteCount)
+    {
+        _bytesRemaining -= byteCount;
+
+#if NET5_0_OR_GREATER
+        _dataReference = ref Unsafe.Add(ref _dataReference, byteCount);
+#else
+        _bufferedByteIndex += byteCount;
+#endif
     }
 }
