dcbb9f89ec1d97d23d50e33bd79e898954b49d4a
zayg21-pixel <zayg21@gmail.com>
2026-09-26 13:24:04 +0200

    Drop font playground

--- PdfPixel.Benchmarks/Program.cs
+++ PdfPixel.Benchmarks/Program.cs
@@ -1,11 +1,4 @@
-using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
-using PdfPixel.Jpx.Decoding;
-using PdfPixel.Jpx.Model;
-using PdfPixel.Jpx.Parsing;
-using System;
-using System.Diagnostics;
-using System.IO;
 
 namespace Benchmarks
 {
@@ -13,15 +6,6 @@ namespace Benchmarks
     {
         public static void Main(string[] args)
         {
-            // test, do not remove.
-            //var widePen = new CurvesWidePenBenchmark();
-            //widePen.Setup();
-
-            //for (int i = 0; i < 10000; i++)
-            //{
-            //    widePen.PdfPixel();
-            //}
-
             BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
         }
     }
